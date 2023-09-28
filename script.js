a585c76f5eff098bb0066af3dc68c12924a55a6f
Ram181999 <141997273+Ram181999@users.noreply.github.com>
2023-09-28 20:58:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 //your JS code here. If required.
+// Get all buttons with the class "btn"
+const buttons = document.querySelectorAll('.btn');
+const stopButton = document.querySelector('.stop');
+const audioElements = {};
+
+// Load audio elements
+buttons.forEach(button => {
+    const soundFile = button.getAttribute('data-sound');
+    const audio = new Audio(`sounds/${soundFile}`);
+    audioElements[soundFile] = audio;
+
+    // Add click event listener to each button
+    button.addEventListener('click', () => {
+        stopAllSounds();
+        audio.play();
+    });
+});
+
+// Add click event listener to the stop button
+stopButton.addEventListener('click', () => {
+    stopAllSounds();
+});
+
+// Function to stop all playing sounds
+function stopAllSounds() {
+    for (const sound in audioElements) {
+        if (audioElements.hasOwnProperty(sound)) {
+            audioElements[sound].pause();
+            audioElements[sound].currentTime = 0;
+        }
+    }
+}
+
